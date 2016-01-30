fc580415569115fac3bcdf83e3d8931b4115d3fc
Olivier Boucher <olivier@bouchernolet.com>
2016-01-30 13:24:44 -0500

    access control and stores concatenation

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,43 @@ var request = require('request');
 var constants = require('../models/constants');
 var router = express.Router();
 
+router.get('/stores/:postalCode', function(req, res){
+	var serviceUrl = constants.SERVICE_URL + '/api/stores/' + req.params.postalCode;
+	console.log({
+		postalCode : req.params.postalCode
+	});
+	request(serviceUrl, function(error, response, body){
+		if(error){
+			console.log('Error making http request to service');
+			res.status(500).json({
+				error: 'Internal server error, service is probably down.',
+				message: responseBody.message
+			});
+		}
+		else {
+			switch(response.statusCode){
+				case 200:
+					res.json(JSON.parse(body));
+					break;
+				case 400:
+					res.status(400).json({
+						error: 'Bad request, verify that the parameters are ok.',
+						message: body
+					});
+					break;
+				case 500:
+					res.status(500).json({
+						error: 'Internal server error, service is probably down.',
+						message: body
+					});
+					break;
+			}
+		}
+	});
+});
+
 router.get('/stores/:bannerCode/:postalCode', function(req, res){
-	var serviceUrl = constants.SERVICE_URL + '/api/' + req.params.bannerCode + '/' + req.params.postalCode;
+	var serviceUrl = constants.SERVICE_URL + '/api/stores/' + req.params.bannerCode + '/' + req.params.postalCode;
 	console.log({
 		bannerCode : req.params.bannerCode,
 		postalCode : req.params.postalCode
