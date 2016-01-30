c7ef9e69f89fe6a75b2086b56a99c9c674d45d54
Olivier Boucher <olivier@bouchernolet.com>
2016-01-30 13:30:02 -0500

    refactoring :)

--- routes/api.js
+++ routes/api.js
@@ -3,38 +3,43 @@ var request = require('request');
 var constants = require('../models/constants');
 var router = express.Router();
 
+
+var handleStoresRequest = function(res, error, response, body){
+	if(error){
+		console.log('Error making http request to service');
+		res.status(500).json({
+			error: 'Internal server error, service is probably down.',
+			message: responseBody.message
+		});
+	}
+	else {
+		switch(response.statusCode){
+			case 200:
+				res.json(JSON.parse(body));
+				break;
+			case 400:
+				res.status(400).json({
+					error: 'Bad request, verify that the parameters are ok.',
+					message: body
+				});
+				break;
+			case 500:
+				res.status(500).json({
+					error: 'Internal server error, service is probably down.',
+					message: body
+				});
+				break;
+		}
+	}
+};
+
 router.get('/stores/:postalCode', function(req, res){
 	var serviceUrl = constants.SERVICE_URL + '/api/stores/' + req.params.postalCode;
 	console.log({
 		postalCode : req.params.postalCode
 	});
-	request(serviceUrl, function(error, response, body){
-		if(error){
-			console.log('Error making http request to service');
-			res.status(500).json({
-				error: 'Internal server error, service is probably down.',
-				message: responseBody.message
-			});
-		}
-		else {
-			switch(response.statusCode){
-				case 200:
-					res.json(JSON.parse(body));
-					break;
-				case 400:
-					res.status(400).json({
-						error: 'Bad request, verify that the parameters are ok.',
-						message: body
-					});
-					break;
-				case 500:
-					res.status(500).json({
-						error: 'Internal server error, service is probably down.',
-						message: body
-					});
-					break;
-			}
-		}
+	request(serviceUrl, function(err, response, body){
+		handleStoresRequest(res, err, response, body);
 	});
 });
 
@@ -44,34 +49,8 @@ router.get('/stores/:bannerCode/:postalCode', function(req, res){
 		bannerCode : req.params.bannerCode,
 		postalCode : req.params.postalCode
 	});
-	request(serviceUrl, function(error, response, body){
-		if(error){
-			console.log('Error making http request to service');
-			res.status(500).json({
-				error: 'Internal server error, service is probably down.',
-				message: responseBody.message
-			});
-		}
-		else {
-			switch(response.statusCode){
-				case 200:
-					res.json(JSON.parse(body));
-					break;
-				case 400:
-					res.status(400).json({
-						error: 'Bad request, verify that the parameters are ok.',
-						message: body
-					});
-					break;
-				case 500:
-					res.status(500).json({
-						error: 'Internal server error, service is probably down.',
-						message: body
-					});
-					break;
-			}
-		}
-
+	request(serviceUrl, function(err, response, body){
+		handleStoresRequest(res, err, response, body);
 	});
 });
 
