bff8a24c9a14b0be5cd53f416f8d5a9005bcc040
Olivier Boucher <olivier@bouchernolet.com>
2016-01-30 13:43:34 -0500

    updated constants

--- models/constants.js
+++ models/constants.js
@@ -1,3 +1,10 @@
  module.exports = {
-	SERVICE_URL: "http://0.0.0.0:1337"
+	SERVICE_URL: "http://0.0.0.0:1337",
+	STORES: {
+		MAXI : "MAXI",
+		LOBLAWS: "LOBLAWS",
+		SUPER_C: "SUPERC",
+		IGA: "IGA",
+		METRO: "METRO"
+	}
  };
