3ef5dc6a56d2d2fe045aabafc5cd8ac19df501d1
Olivier Boucher <olivier@bouchernolet.com>
2016-01-31 00:58:45 -0500

    type

--- routes/api.js
+++ routes/api.js
@@ -36,42 +36,42 @@ var EnhancedRouter = function(type){
 	var that = this;
 
 	this.router.get('/stores/:postalCode', function(req, res){
-		var serviceUrl = constants.SERVICE_URL + '/api/stores/' + req.params.postalCode;
+		var serviceUrl = constants.SERVICE_URL + '/api/stores/' + req.params.postalCode + '?type=' + that.type;
 		request(serviceUrl, function(err, response, body){
 			that.handleServiceResponse(res, err, response, body);
 		});
 	});
 
 	this.router.get('/stores/:bannerCode/:postalCode', function(req, res){
-		var serviceUrl = constants.SERVICE_URL + '/api/stores/' + req.params.bannerCode + '/' + req.params.postalCode;
+		var serviceUrl = constants.SERVICE_URL + '/api/stores/' + req.params.bannerCode + '/' + req.params.postalCode + '?type=' + that.type;
 		request(serviceUrl, function(err, response, body){
 			that.handleServiceResponse(res, err, response, body);
 		});
 	});
 
 	this.router.get('/publications/:bannerCode/:storeGuid', function(req, res) {
-		var serviceUrl = constants.SERVICE_URL + '/api/publications/' + req.params.bannerCode + '/' + req.params.storeGuid;
+		var serviceUrl = constants.SERVICE_URL + '/api/publications/' + req.params.bannerCode + '/' + req.params.storeGuid + '?type=' + that.type;
 		request(serviceUrl, function(err, response, body){
 			that.handleServiceResponse(res, err, response, body);
 		});
 	});
 
 	this.router.get('/products/:publicationId/:productId', function(req, res){
-		var serviceUrl = constants.SERVICE_URL + '/api/products/' + req.params.publicationId + '/' + req.params.productId;
+		var serviceUrl = constants.SERVICE_URL + '/api/products/' + req.params.publicationId + '/' + req.params.productId + '?type=' + that.type;
 		request(serviceUrl, function(err, response, body){
 			that.handleServiceResponse(res, err, response, body);
 		});
 	});
 
 	this.router.get('/closest/publications/:postalCode', function(req, res){
-		var serviceUrl = constants.SERVICE_URL + '/api/closest/publications/' + req.params.postalCode;
+		var serviceUrl = constants.SERVICE_URL + '/api/closest/publications/' + req.params.postalCode + '?type=' + that.type;
 		request(serviceUrl, function(err, response, body){
 			that.handleServiceResponse(res, err, response, body);
 		});
 	});
 
 	this.router.get('/closest/categories/:postalCode', function(req, res){
-		var serviceUrl = constants.SERVICE_URL + '/api/closest/categories/' + req.params.postalCode;
+		var serviceUrl = constants.SERVICE_URL + '/api/closest/categories/' + req.params.postalCode + '?type=' + that.type;
 		request(serviceUrl, function(err, response, body){
 			that.handleServiceResponse(res, err, response, body);
 		});
